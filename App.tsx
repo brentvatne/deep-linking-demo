8abb56b9bd27bb73113f960d8105f369fde18228
Brent Vatne <brentvatne@gmail.com>
2023-05-24 18:04:43 -0700

    Hooks

--- App.tsx
+++ App.tsx
@@ -4,119 +4,115 @@ import * as Linking from "expo-linking";
 import * as WebBrowser from "expo-web-browser";
 import Constants from "expo-constants";
 
-// if (Constants.executionEnvironment === 'storeClient') {
-//   throw new Error('This example is meant to be run either with expo-dev-client or in a standalone app.')
-// }
+export default function App() {
+  const [redirectUrl, setRedirectUrl] = React.useState(null);
+  const [redirectData, setRedirectData] = React.useState(null);
+  const [getInitialURLData, setGetInitialURLData] = React.useState(null);
 
-export default class App extends React.Component {
-  state = {
-    redirectUrl: null,
-    redirectData: null,
-    getInitialURLData: null,
-  };
+  console.log({
+    redirectUrl,
+    redirectData,
+    getInitialURLData,
+  })
 
-  componentDidMount() {
+  React.useEffect(() => {
     Linking.getInitialURL().then((url) => {
-      this.setState({ getInitialURLData: url });
+      setGetInitialURLData(url);
     });
-  }
+  }, [setGetInitialURLData]);
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.subheader}>Linking.getInitialURL()</Text>
-        <Text style={styles.debug}>{this.state.getInitialURLData}</Text>
-        <Text style={styles.subheader}>Linking.createURL('/')</Text>
-        <Text style={styles.debug}>{Linking.createURL("/")}</Text>
-        <Text style={styles.subheader}>Scheme://</Text>
-        <Text style={styles.debug}>{Constants.expoConfig.scheme}://</Text>
+  React.useEffect(() => {
+    let listener = Linking.addEventListener("url", _handleRedirect);
+    return () => {
+      listener.remove();
+    };
+  }, []);
 
-        <View style={styles.separator} />
+  const _handleRedirect = React.useCallback(
+    (event) => {
+      if (Constants.platform.ios) {
+        WebBrowser.dismissBrowser();
+      }
 
-        <Button onPress={this._openBrowserAsync} title="Try openBrowserAsync" />
+      let data = Linking.parse(event.url);
 
-        <Button
-          onPress={this._openAuthSessionAsync}
-          title="Try openAuthSessionAsync"
-        />
-
-        {this._maybeRenderRedirectData()}
-      </View>
-    );
-  }
-
-  _handleRedirect = (event) => {
-    if (Constants.platform.ios) {
-      WebBrowser.dismissBrowser();
-    }
-
-    let data = Linking.parse(event.url);
-
-    this.setState({ redirectData: data, redirectUrl: event.url });
-  };
+      setRedirectData(data);
+      setRedirectUrl(event.url);
+    },
+    [setRedirectData, setRedirectUrl]
+  );
 
   // openAuthSessionAsync doesn't require that you add Linking listeners, it
   // returns the redirect URL in the resulting Promise
-  _openAuthSessionAsync = async () => {
+  const _openAuthSessionAsync = React.useCallback(async () => {
     try {
       let result = await WebBrowser.openAuthSessionAsync(
         // We add `?` at the end of the URL since the test backend that is used
         // just appends `authToken=<token>` to the URL provided.
         `https://backend-xxswjknyfi.now.sh/?linkingUri=${Linking.createURL(
-          "/?"
+          "?"
         )}`
       );
       let redirectData;
       if (result.type === "success" && result.url) {
         redirectData = Linking.parse(result.url);
-        this.setState({ result, redirectData, redirectUrl: result.url });
+        setRedirectData(redirectData);
+        setRedirectUrl(result.url);
       }
     } catch (error) {
       alert(error);
       console.log(error);
     }
-  };
+  }, []);
 
-  // openBrowserAsync requires that you subscribe to Linking events and the
-  // resulting Promise only contains information about whether it was canceled
-  // or dismissed
-  _openBrowserAsync = async () => {
-    let listener = Linking.addEventListener("url", this._handleRedirect);
+  const _openBrowserAsync = React.useCallback(async () => {
     try {
       let result = await WebBrowser.openBrowserAsync(
         // We add `?` at the end of the URL since the test backend that is used
         // just appends `authToken=<token>` to the URL provided.
         `https://backend-xxswjknyfi.now.sh/?linkingUri=${Linking.createURL(
-          "/?"
+          "?"
         )}`
       );
 
-      this.setState({ result });
+      console.log(result);
     } catch (error) {
       alert(error);
       console.log(error);
-    } finally {
-      listener.remove();
     }
-  };
+  }, []);
 
-  _maybeRenderRedirectData = () => {
-    if (!this.state.redirectData) {
-      return;
-    }
+  return (
+    <View style={styles.container}>
+      <Text style={styles.subheader}>Linking.getInitialURL()</Text>
+      <Text style={styles.debug}>{getInitialURLData}</Text>
+      <Text style={styles.subheader}>Linking.createURL('')</Text>
+      <Text style={styles.debug}>{Linking.createURL("")}</Text>
+      <Text style={styles.subheader}>Linking.createURL('?')</Text>
+      <Text style={styles.debug}>{Linking.createURL("?")}</Text>
+      <Text style={styles.subheader}>Scheme://</Text>
+      <Text style={styles.debug}>{Constants.expoConfig.scheme}://</Text>
 
-    return (
-      <>
-        <View style={styles.separator} />
-        <Text style={styles.subheader}>Redirect URL</Text>
-        <Text style={styles.debug}>{this.state.redirectUrl}</Text>
-        <Text style={styles.subheader}>Linking.parse(redirectUrl)</Text>
-        <Text style={styles.debug}>
-          {JSON.stringify(this.state.redirectData)}
-        </Text>
-      </>
-    );
-  };
+      <View style={styles.separator} />
+
+      <Button onPress={_openBrowserAsync} title="Try openBrowserAsync" />
+
+      <Button
+        onPress={_openAuthSessionAsync}
+        title="Try openAuthSessionAsync"
+      />
+
+      {redirectData ? (
+        <>
+          <View style={styles.separator} />
+          <Text style={styles.subheader}>Redirect URL</Text>
+          <Text style={styles.debug}>{redirectUrl}</Text>
+          <Text style={styles.subheader}>Linking.parse(redirectUrl)</Text>
+          <Text style={styles.debug}>{JSON.stringify(redirectData)}</Text>
+        </>
+      ) : null}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
